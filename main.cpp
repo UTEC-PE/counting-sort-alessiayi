7cf3e1cce89729c4deed46e3d2c2f4bd09c89f74
Alessia Yi <alessiayi@Ale-Yi-2.local>
2018-09-20 15:43:23 -0500

    Terminado

--- main.cpp
+++ main.cpp
@@ -3,28 +3,47 @@
 
 using namespace std;
 
-void print(int numbers[], int size);
-void countingSort(int numbers[], int size);
+void print(int numbers[], int size){
+  for (int i=0; i<size; i++){
+    cout << numbers[i] << " ";
+  }
+  cout << endl;
+};
+
+void countingSort(int numbers[], int size){
+  int min=*min_element(numbers,(numbers+size));
+  int index=(*max_element(numbers,(numbers+size)) - min)+1;
+  int arr[index];
+  for (int i=0; i<index; i++){
+    arr[i]=0;
+  }
+
+  for (int i=0; i<size; i++){
+    arr[numbers[i]-min]+=1;
+  }
+
+  for (int i=0; i<index-1; i++){
+    arr[i+1]+=arr[i];
+  }
+
+  int sort[size];
+  for (int i=0; i<size; i++){
+    sort[arr[numbers[i]-min]-=1]=numbers[i];
+  }
+
+  for (int i=0; i<size; i++){
+    numbers[i]=sort[i];
+  }
+};
 
 int main(int argc, char *argv[]) {
-    int numbers[] = {1, 4, 1, 2, 7, 5, 2};
-    int size = sizeof(numbers) / sizeof(*numbers);
+  int numbers[] = {1, 4, 1, 2, 7, 5, 2};
+  int size = sizeof(numbers) / sizeof(*numbers);
 
-    print(numbers, size);
-    countingSort(numbers, size);
-    print(numbers, size);
-    
-    system("read");
-    return EXIT_SUCCESS;
-}
+  print(numbers, size);
+  countingSort(numbers, size);
+  print(numbers, size);
 
-void print(int numbers[], int size) {
-    for (int i = 0; i < size; i++) {
-        cout << numbers[i] << ' ';
-    }
-    cout << endl;
-}
-
-void countingSort(int numbers[], int size) {
-    // TODO
+  system("read");
+  return EXIT_SUCCESS;
 }
